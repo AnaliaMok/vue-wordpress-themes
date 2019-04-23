de714a4bc2fb74576a4b6e2dd2b8a2103cee4874
Analia Mok <analiam319@gmail.com>
2019-04-22 22:41:32 -0400

    :alembic: Configured babelrc file. Converted Post and PageBanner to use JSX.

--- vue-jsx/assets/vue/components/PageBanner.vue
+++ vue-jsx/assets/vue/components/PageBanner.vue
@@ -1,18 +1,8 @@
-<template functional>
-	<div class="img-wrapper relative md:w-4/5 mx-auto">
-		<img :src="props.banner.url" :alt="props.banner.alt" class="block w-full">
-		<div class="absolute pin bg-grey-darkest opacity-50"></div>
-    <div class="content inline-block absolute z-10 text-grey-lightest text-center px-4 lg:px-0 w-5/6 lg:w-3/5">
-      <h1 class="font-display text-xl md:text-4xl mb-2">{{ props.title }}</h1>
-      <p v-if="props.subheading" v-html="props.subheading"></p>
-    </div>
-	</div>
-</template>
-
 <script>
 import { Placeholders } from '@/helpers/constants.js';
 
 export default {
+  functional: true,
   props: {
     banner: Object,
     title: String,
@@ -40,6 +30,27 @@ export default {
             : this.defaultBannerImage.alt,
       };
     },
+  },
+  render(h, context) {
+    let { banner, title, subheading } = context.props;
+    let subheadingMarkup;
+
+    if (subheading) {
+      subheadingMarkup = <p domPropsInnerHTML={subheading} />;
+    } else {
+      subheadingMarkup = null;
+    }
+
+    return (
+      <div class="img-wrapper relative md:w-4/5 mx-auto">
+        <img src={banner.url} alt={banner.alt} class="block w-full" />
+        <div class="absolute pin bg-grey-darkest opacity-50" />
+        <div class="content inline-block absolute z-10 text-grey-lightest text-center px-4 lg:px-0 w-5/6 lg:w-3/5">
+          <h1 class="font-display text-xl md:text-4xl mb-2">{title}</h1>
+          {subheadingMarkup}
+        </div>
+      </div>
+    );
   },
 };
 </script>
--- vue-jsx/assets/vue/templates/Singles/Post.vue
+++ vue-jsx/assets/vue/templates/Singles/Post.vue
@@ -1,20 +1,8 @@
-<template>
-	<Layout>
-		<article>
-			<PageBanner :banner="featuredMedia" :title="title" :subheading="bannerCaption" />
-			<div class="px-4 lg:px-0 md:w-4/5 mx-auto mt-8" v-html="content"></div>
-		</article>
-	</Layout>
-</template>
-
 <script>
 import PageBanner from '@/components/PageBanner.vue';
 import { Placeholders } from '@/helpers/constants.js';
 
 export default {
-  components: {
-    PageBanner,
-  },
   props: {
     title: String,
     content: String,
@@ -44,6 +32,24 @@ export default {
       return `Published on ${this.date} by <a href='${this.author.link}'>${this
         .author.name || 'Unknown'}</a>`;
     },
+  },
+  render: function(h, context) {
+    // TODO: Refactor names in favor of object spreading.
+    return (
+      <Layout>
+        <article>
+          <PageBanner
+            banner={this.featuredMedia}
+            title={this.title}
+            subheading={this.bannerCaption}
+          />
+          <div
+            class="px-4 lg:px-0 md:w-4/5 mx-auto mt-8"
+            domPropsInnerHTML={this.content}
+          />
+        </article>
+      </Layout>
+    );
   },
 };
 </script>
